ca6e6338429a895928a1e5c65f75917ddd7d0f67
Massimiliano Arione <garakkio@gmail.com>
2013-10-30 13:56:07 +0100

    refactoring in login action

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -13,34 +13,26 @@ use Symfony\Component\HttpFoundation\Request;
 class AuthController extends Controller
 {
     /**
+     * Login form
+     *
      * @Route("/login", name="login")
      * @Template()
      */
     public function loginAction(Request $request)
     {
         if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->redirect($this->generateUrl('admin'));
-        }
-        $session = $request->getSession();
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
-        // see https://github.com/symfony/symfony/issues/837#issuecomment-3000155
-        if ($error instanceof \Exception && !$error instanceof BadCredentialsException) {
-            $this->get('logger')->log('error', $error->getMessage());
-            $error = array('message' => 'Unexpected error.');
+            return $this->redirect($this->generateUrl('homepage'));
         }
 
         return array(
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-            'error'         => $error,
+            'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $this->getLoginError($request),
         );
     }
 
     /**
+     * Logout (implemented by Symfony security system)
+     *
      * @Route("/logout", name="logout")
      */
     public function logoutAction()
@@ -48,11 +40,34 @@ class AuthController extends Controller
     }
 
     /**
+     * Login check (implemented by Symfony security system)
+     *
      * @Route("/login_check", name="login_check")
      * @Method("POST")
      */
     public function loginCheckAction()
     {
         throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
+    }
+
+    /**
+     * Get possible authentication error
+     *
+     * @param  Request $request
+     * @return mixed            Exception or array
+     */
+    protected function getLoginError(Request $request)
+    {
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+            $request->getSession()->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        // see https://github.com/symfony/symfony/issues/837#issuecomment-3000155
+        if ($error instanceof \Exception && !$error instanceof BadCredentialsException) {
+            $this->get('logger')->log('error', $error->getMessage());
+            $error = array('message' => 'Unexpected error.');
+        }
     }
 }
